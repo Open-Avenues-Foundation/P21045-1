44f86339d88a87842a7bce82b1c1bb4513acd3ed
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2022-01-04 16:27:34 -0500

    attempt at csv

---
+++ customers.js
@@ -0,0 +1,13 @@
+const customers = [{
+  firstName: ('Ying'),
+  lastName: ('Lu'),
+  email: ('Comcast.net'),
+  city: ('Sharon'),
+  zipCode: ('02067'),
+  phoneNumber: ('781-799-2641'),
+
+}]
+
+module.exports = {
+  customers
+}
---
+++ index.js
@@ -0,0 +1,9 @@
+
+const express = require('express')
+
+const app = express()
+
+app.listen(1365, () => {
+  // eslint-disable-next-line no-console
+  console.log('listening On Port 1365...')
+})
